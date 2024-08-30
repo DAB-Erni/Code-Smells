360410807caa27225b5454033be3099121931cb3
Diane <diane.besmonte@betterask.erni>
2024-08-30 16:29:00 +0800

    Added error handling for getting users

--- Code Smells Exercise/Layered-Design/Program.cs
+++ Code Smells Exercise/Layered-Design/Program.cs
@@ -7,14 +7,28 @@ class Program
         UserService userService = new UserService();
 
         var users = await userService.GetAllUsersAsync();
-        foreach (var user in users.RootElement.EnumerateArray())
+        try
         {
-            int id = user.GetProperty("id").GetInt32();
-            string name = user.GetProperty("name").GetString() ?? "Guest";
-            string company = user.GetProperty("company").GetString() ?? "N/A";
+            if (users != null)
+            {
+                foreach (var user in users.RootElement.EnumerateArray())
+                {
+                    int id = user.GetProperty("id").GetInt32();
+                    string name = user.GetProperty("name").GetString() ?? "Guest";
+                    string company = user.GetProperty("company").GetString() ?? "N/A";
 
 
-            Console.WriteLine($"id: {id}, name: {name}, company: {company}");
+                    Console.WriteLine($"id: {id}, name: {name}, company: {company}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No users found.");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Request error: , {ex.Message}");
         }
     }
 }
